f1b6331f8fee3ad85d418fb1d5a5e20408b7ab3a
Alex Eagle <alex@aspect.dev>
2024-02-17 12:31:54 -0800

    update repro to show it's fixed now

--- repro.sh
+++ repro.sh
@@ -4,6 +4,7 @@ set -o errexit -o nounset
 TMP=$(mktemp -d)
 mkdir -p $TMP/src/modules/moduleA
 mkdir -p $TMP/src/modules/moduleB
+mkdir -p $TMP/bazel-out/arch/bin
 
 ln -s $PWD/.swcrc $TMP
 ln -s $PWD/src/index.ts $TMP/src
@@ -12,5 +13,10 @@ ln -s $PWD/src/modules/moduleB/index.ts $TMP/src/modules/moduleB
 
 cd $TMP
 ls -alR
-~/Downloads/swc-darwin-arm64 compile --source-maps false --config-file .swcrc --out-file src/index.js src/index.ts
-grep require src/index.js
+wget --quiet --output-document swc https://github.com/swc-project/swc/releases/download/v1.3.107/swc-darwin-arm64
+chmod u+x swc
+xattr -c swc
+
+./swc --version
+./swc compile --source-maps false --config-file .swcrc --out-file bazel-out/arch/bin/src/index.js src/index.ts
+grep require bazel-out/arch/bin/src/index.js
