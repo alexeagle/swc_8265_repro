16bfa92286bdafe5dc48ead53c9b4177ce603fbb
Jason Bedard <jason@aspect.dev>
2024-02-20 14:35:04 -0800

    update test case to align with bazel bin+sandbox directories

--- repro.sh
+++ repro.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset
 
-TMP=$(mktemp -d)
-mkdir -p $TMP/src/modules/moduleA
-mkdir -p $TMP/src/modules/moduleB
-mkdir -p $TMP/bazel-out/arch/bin
+OUTPUT_BASE=$(mktemp -d)
+BINDIR=$OUTPUT_BASE/bazel-out/arch/bin
+SANDBOX=$OUTPUT_BASE/sandbox/123
 
-ln -s $PWD/.swcrc $TMP
-ln -s $PWD/src/index.ts $TMP/src
-ln -s $PWD/src/modules/moduleA/index.ts $TMP/src/modules/moduleA
-ln -s $PWD/src/modules/moduleB/index.ts $TMP/src/modules/moduleB
+mkdir -p $BINDIR/src/modules/moduleA
+mkdir -p $BINDIR/src/modules/moduleB
+mkdir -p $SANDBOX/src/modules/moduleA
+mkdir -p $SANDBOX/src/modules/moduleB
 
-cd $TMP
+# hard links from BINDIR into src
+ln $PWD/.swcrc $BINDIR
+ln $PWD/src/index.ts $BINDIR/src
+ln $PWD/src/modules/moduleA/index.ts $BINDIR/src/modules/moduleA
+ln $PWD/src/modules/moduleB/index.ts $BINDIR/src/modules/moduleB
+
+# soft links from sandbox into bazel-bin
+ln -s $BINDIR/.swcrc $SANDBOX
+ln -s $BINDIR/src/index.ts $SANDBOX/src
+ln -s $BINDIR/src/modules/moduleA/index.ts $SANDBOX/src/modules/moduleA
+ln -s $BINDIR/src/modules/moduleB/index.ts $SANDBOX/src/modules/moduleB
+
+echo "OUTPUT_BASE: $OUTPUT_BASE"
+cd $SANDBOX
 ls -alR
 wget --quiet --output-document swc https://github.com/swc-project/swc/releases/download/v1.3.107/swc-darwin-arm64
 chmod u+x swc
 xattr -c swc
 
 ./swc --version
-./swc compile --source-maps false --config-file .swcrc --out-file bazel-out/arch/bin/src/index.js src/index.ts
-grep require bazel-out/arch/bin/src/index.js
+./swc compile --source-maps false --config-file .swcrc --out-file $BINDIR/src/index.js src/index.ts
+grep require $BINDIR/src/index.js
